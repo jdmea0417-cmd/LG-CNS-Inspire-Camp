a0b1ac43d33193bc3339dd5140625fcb3cb4ee8a
jdmea <jdmea0417@gmail.com>
2026-05-26 17:30:09 +0900

    Resolve location conflicts by removing old directories

---
+++ am_inspire_react_todo/src/App.js
@@ -0,0 +1,8 @@
+import logo from './logo.svg';
+import './App.css';
+
+export const App = () => {
+  return (
+    <center>섭섭이와 함께하는 즐거운 React(feat. javascript)</center>
+  );
+}
--- script/main.js
+++
@@ -1,6 +0,0 @@
-// import {} : 가져오고자 하는 함수가 named export 형태일 때
-// default export일 경우, 중괄호 제외
-import {add} from './sub.js'
-
-
-console.log(`debug >>>> module import ` , add(4, 5))
